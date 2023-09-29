113d9969a24d77e6cb39ad9ae1b201d3d17a8d2d
Andreea <andreeapopa194@gmail.com>
2023-09-29 20:54:38 +0300

    Instantiate UI elements

--- app/src/main/java/ro/andreeapopa/basiccalculator/MainActivity.java
+++ app/src/main/java/ro/andreeapopa/basiccalculator/MainActivity.java
@@ -1,14 +1,46 @@
 package ro.andreeapopa.basiccalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvResult, tvHistory;
+    private AppCompatButton btnZero, btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine,
+                            btnMinus, btnPlus, btnDivide, btnMultiply, btnEquals, btnPoint, btnAC, btnDEL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instantiateElements();
+    }
+
+    private void instantiateElements() {
+        btnZero = findViewById(R.id.btnZero);
+        btnOne = findViewById(R.id.btnOne);
+        btnTwo = findViewById(R.id.btnTwo);
+        btnThree = findViewById(R.id.btnThree);
+        btnFour = findViewById(R.id.btnFour);
+        btnFive = findViewById(R.id.btnFive);
+        btnSix = findViewById(R.id.btnSix);
+        btnSeven = findViewById(R.id.btnSeven);
+        btnEight = findViewById(R.id.btnEight);
+        btnNine = findViewById(R.id.btnNine);
+
+        btnMinus = findViewById(R.id.btnMinus);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnEquals = findViewById(R.id.btnEquals);
+        btnPoint = findViewById(R.id.btnPoint);
+        btnAC = findViewById(R.id.btnAC);
+        btnDEL = findViewById(R.id.btnDEL);
+
+        tvResult = findViewById(R.id.tvResult);
+        tvHistory = findViewById(R.id.tvHistory);
     }
 }
